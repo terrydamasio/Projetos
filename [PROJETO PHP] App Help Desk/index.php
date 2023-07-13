f60e5283b5e434d8a68782e4870b9b8c0d7a24c9
Terry Damasio Santos <terry@lifecti.com.br>
2023-07-13 16:35:28 -0300

    Atualização bootstrap do projeto app help desk

--- [PROJETO PHP] App Help Desk/index.php
+++ [PROJETO PHP] App Help Desk/index.php
@@ -10,11 +10,51 @@
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- Style CSS -->
     <style>
-      .card-login {
-        padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
+      form {
+        width: 30%;
+        background-color: #f0f0f0;
+        padding: 50px;
+        border-radius: 20px;
+        margin-top: 150px;
       }
+
+      .btn-info {
+        width: 100%;
+        border: none;
+        border-radius: 50px;
+      }
+
+      .form-control {
+        background-color: #f0f0f0;
+        color: rgba(0, 0, 0, .87);
+        border-bottom-color: rgba(0, 0, 0, .42);
+        box-shadow: none !important;
+        border: none;
+        border-bottom: 1px solid;
+        border-radius: 4px 4px 0 0;
+      }
+
+      h4 {
+        font-size: 2rem !important;
+        font-weight: 700;
+      }
+
+      .form-label {
+        font-family: 800 !important;
+      }
+
+      @media only screen and (max-width: 600px) {
+        form {
+          width: 100% !important;
+        }
+      }
+
+      @media (min-width: 601px) and (max-width: 1005px) {
+        form {
+          width: 50% !important;
+        }
+      }
+      
     </style>
   </head>
 
@@ -29,45 +69,40 @@
     <div class="container">    
       <div class="row">
 
-        <div class="card-login">
-          <div class="card">
-            <div class="card-header">
-              Login
+        <div class="container-fluid">
+          <form class="mx-auto" action="valida_login.php" method="post"> <!-- destino do submit do formulário -->
+            <h4 class="text-center">Login</h4>
+            <div class="form-group mb-3">
+              <input name="email" type="email" class="form-control" placeholder="E-mail">
             </div>
-            <div class="card-body">
-              <form action="valida_login.php" method="post"> <!-- destino do submit do formulário -->
-                <div class="form-group">
-                  <input name="email" type="email" class="form-control" placeholder="E-mail">
-                </div>
-                <div class="form-group">
-                  <input name="senha" type="password" class="form-control" placeholder="Senha">
-                </div>
-
-                <?php
-                  if(isset($_GET['login']) && $_GET['login'] == 'erro') {
-                ?>
-   
-                    <div class="text-danger">
-                      Usuário ou senha inválido(s)
-                    </div>
-
-                <?php } ?>
-
-                <?php
-                  if(isset($_GET['login']) && $_GET['login'] == 'erro2') {
-                ?>
-   
-                    <div class="text-danger">
-                     Faça login antes de acessar as páginas protegidas
-                    </div>
-
-                <?php } ?>
-
-                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
-              </form>
+            <div class="form-group mb-3">
+              <input name="senha" type="password" class="form-control" placeholder="Senha">
             </div>
-          </div>
-        </div>
+
+            <?php
+              if(isset($_GET['login']) && $_GET['login'] == 'erro') {
+            ?>
+    
+            <div class="text-danger">
+              Usuário ou senha inválido(s)
+            </div>
+
+            <?php } ?>
+
+            <?php
+              if(isset($_GET['login']) && $_GET['login'] == 'erro2') {
+            ?>
+    
+            <div class="text-danger">
+               Faça login para de acessar as páginas protegidas
+            </div>
+
+            <?php } ?>
+
+            <button class="btn btn-lg btn-info btn-block mt-4" type="submit">Entrar</button>
+          </form>
+        </div>  
+      </div>
     </div>
   </body>
 </html>
