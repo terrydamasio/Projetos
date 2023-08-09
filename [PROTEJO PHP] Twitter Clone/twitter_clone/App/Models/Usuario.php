6a4f7af3c9b81f6cbf5b780ce0effa37f3ab16ef
Terry Damasio <terrydamasio610@gmail.com>
2023-08-09 18:02:59 -0300

    Atualização projeto twitter clone

---
+++ [PROTEJO PHP] Twitter Clone/twitter_clone/App/Models/Usuario.php
@@ -0,0 +1,23 @@
+<?php 
+    namespace App\Models;
+
+    use MF\Model\Model;
+
+    class Usuario extends Model {
+        private $id;
+        private $nome;
+        private $email;
+        private $senha;
+        
+        //salvar
+
+
+        //validar cadastro
+    
+    
+    
+    }
+
+
+
+?>
