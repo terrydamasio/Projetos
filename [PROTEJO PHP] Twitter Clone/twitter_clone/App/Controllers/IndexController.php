674c54142f05544806608ccdc9ee326ee805bb99
Terry Damasio <terrydamasio610@gmail.com>
2023-08-10 15:07:10 -0300

    Início projeto twitter clone

--- [PROTEJO PHP] Twitter Clone/twitter_clone/App/Controllers/IndexController.php
+++ [PROTEJO PHP] Twitter Clone/twitter_clone/App/Controllers/IndexController.php
@@ -14,7 +14,40 @@ class IndexController extends Action {
 	}
 
 	public function inscreverse() {
+		$this->view->erroCadastro = false;
+
 		$this->render('inscreverse');
+	}
+
+	public function registrar() {
+
+		//receber dados do formulario
+		$usuario = Container::getModel('Usuario');
+
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		//verificar se o registro vai ser inserido ou não no db
+		if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
+
+			//quantidade de elementos contida dentro do array
+			$usuario->salvar();
+			$this->render('cadastro');
+
+		} else {
+
+			$this->view->erroCadastro = true;
+
+			$this->render('inscreverse');
+		}
+
+		
+
+		//sucesso
+
+
+		//erro
 	}
 
 }
--- [PROTEJO PHP] Twitter Clone/twitter_clone/App/Models/Usuario.php
+++ [PROTEJO PHP] Twitter Clone/twitter_clone/App/Models/Usuario.php
@@ -8,12 +8,55 @@
         private $nome;
         private $email;
         private $senha;
-        
+
+        public function __get($atributo) {
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+        }
+
         //salvar
+        public function salvar() {
+            $query = "insert into usuarios(nome, email, senha) values(:nome, :email, :senha)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':nome', $this->__get('nome'));
+            $stmt->bindValue(':email', $this->__get('email'));
+            $stmt->bindValue(':senha', $this->__get('senha')); //md4() -> hash 32 caracteres 
+            $stmt->execute();
 
+            return $this;
+        }
 
-        //validar cadastro
-    
+        //validar cadastro (caso o usuario deixe de preencher algum input)
+        public function validarCadastro() {
+            $valido = true;
+
+            if(strlen($this->__get('nome')) < 4) {
+                $valido = false;
+            }
+
+            if(strlen($this->__get('email')) < 3) {
+                $valido = false;
+            }
+
+            if (!preg_match('/[A-Za-z]/', $this->__get('senha')) || !preg_match('/[0-9]/', $this->__get('senha')) || strlen($this->__get('senha')) < 5) {
+                $valido = false;
+            }
+            
+            return $valido;
+        }
+        
+        //recuperar usuário por email
+        public function getUsuarioPorEmail() {
+            $query = "select nome, email from usuarios where email = :email";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindvalue(':email', $this->__get('email'));
+            $stmt->execute();
+
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }
     
     
     }
