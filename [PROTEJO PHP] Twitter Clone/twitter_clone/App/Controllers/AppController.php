26f42808d93dc1a192a8ede72fa4299668e50c34
Terry Damasio <terrydamasio610@gmail.com>
2023-08-14 15:42:23 -0300

    Finalização do projeto twitter clone php

--- [PROTEJO PHP] Twitter Clone/twitter_clone/App/Controllers/AppController.php
+++ [PROTEJO PHP] Twitter Clone/twitter_clone/App/Controllers/AppController.php
@@ -19,7 +19,16 @@ class AppController extends Action {
 
         $tweet->__set('id_usuario', $_SESSION['id']);
 
-        $tweets = $tweet->getAll();
+        //variáveis de paginação 
+		$total_registros_pagina = 10; //limit
+        $pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1; //qual página
+        $deslocamento = ($pagina - 1) * $total_registros_pagina; //offset
+
+        //$tweets = $tweet->getAll();
+        $tweets = $tweet->getPorPagina($total_registros_pagina, $deslocamento);
+        $total_tweets = $tweet->getTotalRegistros();
+        $this->view->total_de_paginas = ceil($total_tweets['total'] / $total_registros_pagina);
+        $this->view->pagina_ativa = $pagina; //exibir pagina ativa na navegação
 
         $this->view->tweets = $tweets;
 
